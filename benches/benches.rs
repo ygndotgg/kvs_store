8881a1c46bd6467e2457a7c5ac760c35f79c1439
Gagan Yarramsetty <gaganyarramsetty@gmail.com>
2026-02-22 20:01:32 +0530

    Comparison function was added

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,8 +1,4 @@
-use std::{
-    net::{Shutdown, SocketAddr, TcpListener, TcpStream},
-    thread,
-    time::Duration,
-};
+use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
 
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use crossbeam_utils::sync::WaitGroup;
@@ -76,14 +72,8 @@ fn sled_read(c: &mut Criterion) {
 }
 
 pub fn thread_counts() -> Vec<u32> {
-    let cpu = num_cpus::get() as u32;
-    let mut counts = vec![1, 2, 4];
-    let mut n = 6;
-    while n <= cpu * 2 {
-        counts.push(n);
-        n += 2;
-    }
-    counts
+    // Reduced for faster benchmarking
+    vec![1, 4, num_cpus::get() as u32, 32]
 }
 
 pub fn send_request(addr: SocketAddr, request: &Request) -> Response {
@@ -246,6 +236,274 @@ fn write_rayon_sled(c: &mut Criterion) {
     group.finish();
 }
 
+fn comparison(c: &mut Criterion) {
+    let mut group = c.benchmark_group("comparison");
+    let inputs = thread_counts();
+    for ine in inputs {
+        group.throughput(criterion::Throughput::Bytes(ine as u64));
+
+        group.bench_with_input(
+            BenchmarkId::new("write_rayon_kvstore", ine),
+            &ine,
+            |b, &inde| {
+                let temp_dir = TempDir::new().unwrap();
+                let addr = available_port();
+                let engine = KvStore::open(temp_dir.path()).expect("Unable to Start the engine");
+                let server_pool = SharedQueueThreadPool::new(inde)
+                    .expect("unable to create the server thread pool");
+                let server =
+                    KvServer::new(addr, engine, server_pool).expect("unable to create the server");
+
+                let shutdown_handle = server.shutdown_handle();
+                std::thread::spawn(move || {
+                    server.run();
+                });
+
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+
+                let client_pool =
+                    RayonThreadPool::new(100).expect("Unable to create the client thread pool");
+                b.iter(|| {
+                    let wg = WaitGroup::new();
+                    for i in 0..100 {
+                        let wg = wg.clone();
+                        client_pool.spawn(move || {
+                            let response = send_request(
+                                addr,
+                                &Request::Set {
+                                    key: format!("key{}", i),
+                                    value: "value".to_string(),
+                                },
+                            );
+                            assert!(matches!(response, Response::Ok(_)));
+                            drop(wg);
+                        });
+                    }
+                    wg.wait();
+                });
+                shutdown_handle.store(true, std::sync::atomic::Ordering::SeqCst);
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("write_queued_kvstore", ine),
+            &ine,
+            |b, &inde| {
+                let temp_dir = TempDir::new().unwrap();
+                let addr = available_port();
+                let engine =
+                    SledKvsEngine::open(temp_dir.path()).expect("Unable to Start the engine");
+                let server_pool = SharedQueueThreadPool::new(inde)
+                    .expect("unable to create the server thread pool");
+                let server =
+                    KvServer::new(addr, engine, server_pool).expect("unable to create the server");
+
+                let shutdown_handle = server.shutdown_handle();
+                std::thread::spawn(move || {
+                    server.run();
+                });
+
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+
+                let client_pool = SharedQueueThreadPool::new(100)
+                    .expect("Unable to create the client thread pool");
+                b.iter(|| {
+                    let wg = WaitGroup::new();
+                    for i in 0..100 {
+                        let wg = wg.clone();
+                        client_pool.spawn(move || {
+                            let response = send_request(
+                                addr,
+                                &Request::Set {
+                                    key: format!("key{}", i),
+                                    value: "value".to_string(),
+                                },
+                            );
+                            assert!(matches!(response, Response::Ok(_)));
+                            drop(wg);
+                        });
+                    }
+                    wg.wait();
+                });
+                shutdown_handle.store(true, std::sync::atomic::Ordering::SeqCst);
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("write_rayon_sled", ine),
+            &ine,
+            |b, &inde| {
+                let temp_dir = TempDir::new().unwrap();
+                let addr = available_port();
+                let engine =
+                    SledKvsEngine::open(temp_dir.path()).expect("Unable to Start the engine");
+                let server_pool = SharedQueueThreadPool::new(inde)
+                    .expect("unable to create the server thread pool");
+                let server =
+                    KvServer::new(addr, engine, server_pool).expect("unable to create the server");
+
+                let shutdown_handle = server.shutdown_handle();
+                std::thread::spawn(move || {
+                    server.run();
+                });
+
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+
+                let client_pool = SharedQueueThreadPool::new(100)
+                    .expect("Unable to create the client thread pool");
+                b.iter(|| {
+                    let wg = WaitGroup::new();
+                    for i in 0..100 {
+                        let wg = wg.clone();
+                        client_pool.spawn(move || {
+                            let response = send_request(
+                                addr,
+                                &Request::Set {
+                                    key: format!("key{}", i),
+                                    value: "value".to_string(),
+                                },
+                            );
+                            assert!(matches!(response, Response::Ok(_)));
+                            drop(wg);
+                        });
+                    }
+                    wg.wait();
+                });
+                shutdown_handle.store(true, std::sync::atomic::Ordering::SeqCst);
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("read_rayon_kvstore", ine),
+            &ine,
+            |b, &inde| {
+                let temp_dir = TempDir::new().unwrap();
+                let addr = available_port();
+                let engine = KvStore::open(temp_dir.path()).expect("Unable to Start the engine");
+                let server_pool = SharedQueueThreadPool::new(inde)
+                    .expect("unable to create the server thread pool");
+                let server =
+                    KvServer::new(addr, engine, server_pool).expect("unable to create the server");
+
+                let shutdown_handle = server.shutdown_handle();
+                std::thread::spawn(move || {
+                    server.run();
+                });
+
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+
+                let client_pool =
+                    RayonThreadPool::new(100).expect("Unable to create the client thread pool");
+                b.iter(|| {
+                    let wg = WaitGroup::new();
+                    for i in 0..100 {
+                        let wg = wg.clone();
+                        client_pool.spawn(move || {
+                            let response = send_request(
+                                addr,
+                                &Request::Get {
+                                    key: format!("key{}", i),
+                                },
+                            );
+                            assert!(matches!(response, Response::Ok(_)));
+                            drop(wg);
+                        });
+                    }
+                    wg.wait();
+                });
+                shutdown_handle.store(true, std::sync::atomic::Ordering::SeqCst);
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("read_queued_kvstore", ine),
+            &ine,
+            |b, &inde| {
+                let temp_dir = TempDir::new().unwrap();
+                let addr = available_port();
+                let engine =
+                    SledKvsEngine::open(temp_dir.path()).expect("Unable to Start the engine");
+                let server_pool = SharedQueueThreadPool::new(inde)
+                    .expect("unable to create the server thread pool");
+                let server =
+                    KvServer::new(addr, engine, server_pool).expect("unable to create the server");
+
+                let shutdown_handle = server.shutdown_handle();
+                std::thread::spawn(move || {
+                    server.run();
+                });
+
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+
+                let client_pool = SharedQueueThreadPool::new(100)
+                    .expect("Unable to create the client thread pool");
+                b.iter(|| {
+                    let wg = WaitGroup::new();
+                    for i in 0..100 {
+                        let wg = wg.clone();
+                        client_pool.spawn(move || {
+                            let response = send_request(
+                                addr,
+                                &Request::Get {
+                                    key: format!("key{}", i),
+                                },
+                            );
+                            assert!(matches!(response, Response::Ok(_)));
+                            drop(wg);
+                        });
+                    }
+                    wg.wait();
+                });
+                shutdown_handle.store(true, std::sync::atomic::Ordering::SeqCst);
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("read_rayon_sled", ine),
+            &ine,
+            |b, &inde| {
+                let temp_dir = TempDir::new().unwrap();
+                let addr = available_port();
+                let engine =
+                    SledKvsEngine::open(temp_dir.path()).expect("Unable to Start the engine");
+                let server_pool = SharedQueueThreadPool::new(inde)
+                    .expect("unable to create the server thread pool");
+                let server =
+                    KvServer::new(addr, engine, server_pool).expect("unable to create the server");
+
+                let shutdown_handle = server.shutdown_handle();
+                std::thread::spawn(move || {
+                    server.run();
+                });
+
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+
+                let client_pool = SharedQueueThreadPool::new(100)
+                    .expect("Unable to create the client thread pool");
+                b.iter(|| {
+                    let wg = WaitGroup::new();
+                    for i in 0..100 {
+                        let wg = wg.clone();
+                        client_pool.spawn(move || {
+                            let response = send_request(
+                                addr,
+                                &Request::Get {
+                                    key: format!("key{}", i),
+                                },
+                            );
+                            assert!(matches!(response, Response::Ok(_)));
+                            drop(wg);
+                        });
+                    }
+                    wg.wait();
+                });
+                shutdown_handle.store(true, std::sync::atomic::Ordering::SeqCst);
+            },
+        );
+    }
+    group.finish();
+}
+
 fn read_rayon_kvstore(c: &mut Criterion) {
     let mut group = c.benchmark_group("write_queued_kvstore");
     let inputs = thread_counts();
@@ -388,18 +646,27 @@ fn read_rayon_sled(c: &mut Criterion) {
 }
 // criterion_group!(benches, kvs_write, sled_write, kvs_read, sled_read);
 
+// criterion_group! {
+//     name = benches;
+//     config = Criterion::default()
+//         .sample_size(5)
+//         .warm_up_time(std::time::Duration::from_millis(100))
+//         .measurement_time(std::time::Duration::from_millis(500));
+//     targets =
+//      write_queued_kvstore,
+//               write_rayon_kvstore,
+//               write_rayon_sled,
+//               read_queued_kvstore,
+//               read_rayon_kvstore,
+//               read_rayon_sled
+// }
+
 criterion_group! {
     name = benches;
     config = Criterion::default()
         .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(500))
-        .measurement_time(std::time::Duration::from_secs(1));
-    targets = write_queued_kvstore,
-              write_rayon_kvstore,
-              write_rayon_sled,
-              read_queued_kvstore,
-              read_rayon_kvstore,
-              read_rayon_sled
+        .warm_up_time(std::time::Duration::from_millis(100))
+        .measurement_time(std::time::Duration::from_millis(500));
+    targets = comparison
 }
-
 criterion_main!(benches);
